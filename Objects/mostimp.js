0f87715089a072d59abc2bb1e8aa1200b4f548aa
nilajcoder <nilaj.chakraborty007@gmail.com>
2026-02-09 23:16:04 +0530

    faq

---
+++ Objects/mostimp.js
@@ -0,0 +1,24 @@
+
+
+/*
+
+There three type of alertbox in js
+* alert
+* confirm
+*prompt
+*/
+
+
+/*
+How to Convert Object into array
+*/
+
+let obj={
+
+    a:1,
+    b:2,
+    c:3
+}
+
+let val=Object.values(obj)
+console.log(val)
