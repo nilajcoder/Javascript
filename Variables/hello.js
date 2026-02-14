3dc683b027a5f990022be98c55b23a21a54284ac
nilajcoder <nilaj.chakraborty007@gmail.com>
2026-02-14 21:55:43 +0530

    variable

---
+++ Variables/hello.js
@@ -0,0 +1,38 @@
+
+
+
+/*
+What is Javascript?
+
+Js is a Dynamic Programming Language OOP Light Weighted suppots Both Client side and Server Side
+
+Where we use?
+* To Make Website Interactive
+
+**console.log() is not part of core JavaScript; it is provided by the runtime environment (browser or Node.js).
+
+Browsers provide:
+
+console
+
+console.log()
+
+window
+
+document
+
+Q: Is console.log() JavaScript?
+A: No. It is provided by the execution environment, not by JavaScript itself.
+
+Break the word 🔍
+
+console → a tool provided by the browser / Node.js to see output
+
+log → record / write / show information
+
+So,
+
+console.log() = write information to the console
+*/
+
+console.log("Hello World");  
