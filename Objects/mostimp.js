ede63276df636beda551c9c427cfcfcdf74d8bae
nilajcoder <nilaj.chakraborty007@gmail.com>
2026-02-14 14:36:16 +0530

    faq

--- Objects/mostimp.js
+++ Objects/mostimp.js
@@ -22,3 +22,45 @@ let obj={
 
 let val=Object.values(obj)
 console.log(val)
+
+
+/*
+
+We Have List Of Employee
+List Contains Name ,age, designation,salary
+Calculate D.A 10% of Salary
+and total salary=da+salary
+*/
+
+let empobj=[
+
+    {
+    name: "Nilaj",
+    age:25,
+    designation:"Developer",
+    salary:40000
+
+  },
+
+  {
+    name:"Debobroto",
+    age:40,
+    designation:"Manager",
+    salary:100000
+  }
+
+
+]
+
+console.log(obj.name)//undefined
+for(let i=0;i<empobj.length;i++){
+    console.log(empobj[i].name)
+    console.log(empobj[i].age)
+    console.log(empobj[i].designation)
+
+empobj[i].da=empobj[i].salary*0.10;
+empobj[i].totalsalary=empobj[i].salary+empobj[i].da;
+}
+
+console.log(empobj)
+
