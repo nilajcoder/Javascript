0c208ca7b1f144c371957f160ddea19bb8d922e1
nilajcoder <nilaj.chakraborty007@gmail.com>
2026-02-16 19:46:00 +0530

    dowhile

---
+++ Loops/whiledo.js
@@ -0,0 +1,46 @@
+
+
+/*
+
+while loop
+
+while(condition){
+//code block
+}
+
+*First Condition check
+ if true block execute 
+ if false not execute
+*/
+
+/*
+Print 1 to 5
+*/
+
+console.log("While Loop ")
+let i=1;
+while(i<=5){
+
+    console.log(i)
+    i++
+}
+
+
+/*
+do while
+The block executes first
+
+Then the condition is checked
+
+So it runs at least one time, even if condition is false
+*/
+
+
+console.log("do While")
+
+let j=1;
+do{
+    console.log(j)
+    j++
+
+} while(j<=5)
